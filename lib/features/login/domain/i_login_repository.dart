6a0dd64b49738262e43ed5080f9753872478361a
HardyJL <j.hardenberg@hotmail.de>
2023-05-23 15:53:37 +0200

    firebase

--- lib/features/login/domain/i_login_repository.dart
+++ lib/features/login/domain/i_login_repository.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-abstract interface class ILoginRepository {
+abstract class ILoginRepository {
   FutureOr<void> registerWithEmail(String email, String password);
   FutureOr<void> login(String email, String password);
   FutureOr<void> logout();
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:plan_n_track/app.dart';
 import 'package:plan_n_track/bootstrap.dart';
 
-void main() {
-  bootstrap(() => const App());
+import 'package:plan_n_track/firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await bootstrap(() => const App());
 }
